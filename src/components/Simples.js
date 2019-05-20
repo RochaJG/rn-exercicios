451970318ddbc09edb74e45d1924760e764edf94
Jordan Rocha <70rd23@gmail.com>
2019-05-20 00:40:19 -0300

    add: default style to exercises

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+import Padrao from "../style/Padrao";
+
 export default class Simples extends Component {
   render() {
     return (
       <View>
-        <Text>{this.props.texto}</Text>
+        <Text style={Padrao.ex}>{this.props.texto}</Text>
       </View>
     );
   }
