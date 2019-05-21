7de20d36a473d9ce6896a0c7c1c2f95ff8835379
Jordan Rocha <70rd23@gmail.com>
2019-05-21 11:22:47 -0300

    feat: controlled component concept and first input

--- src/Menu.js
+++ src/Menu.js
@@ -4,9 +4,13 @@ import { createDrawerNavigator } from "react-navigation";
 import Simples from "./components/Simples";
 import Plataformas from "./components/Plataformas";
 import ValidarProps from "./components/ValidarProps";
+import Evento from "./components/Evento";
 
 export default createDrawerNavigator(
   {
+    Evento: {
+      screen: Evento
+    },
     ValidarProps: {
       screen: () => <ValidarProps ano={18} />
     },
